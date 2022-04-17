d3aa151ca2a3a6fcee48b401fa741b95d413c2b9
Ian Ruh <7023667+ianruh@users.noreply.github.com>
2022-04-17 18:49:47 -0500

    cppmpc and symengine dependencies added

---
+++ simulator/src/MPCNonLinearTracking.h
@@ -0,0 +1,40 @@
+#ifndef SRC_INFINITE_LQR_LINEAR_TRACKING_H_
+#define SRC_INFINITE_LQR_LINEAR_TRACKING_H_
+#include "OrbitalState.h"
+#include "Simulator.h"
+#include "TrajectoryGeneration.h"
+#include <memory>
+#include <Eigen/Dense>
+
+namespace Controllers {
+
+class MPCNonLinearTrackingVehicle: public Simulator::Vehicle {
+ private:
+    Eigen::Matrix<double, 6, 6> A;
+    Eigen::Matrix<double, 6, 3> B;
+    Eigen::Matrix<double, 6, 6> Pf;
+    Eigen::Matrix<double, 6, 6> Q;
+    Eigen::Matrix<double, 3, 3> R;
+    double targetSMA;
+
+    Eigen::Matrix<double, 3, 6> K;
+
+    std::shared_ptr<Trajectory> targetTrajectory;
+
+ public:
+
+    MPCNonLinearTrackingVehicle(
+            double mass,
+            Simulator::PV state,
+            double targetSMA,
+            std::shared_ptr<Trajectory> targetTrajectory);
+
+    Eigen::Vector3d getControl(
+            [[maybe_unused]] double t,
+            [[maybe_unused]] const Simulator::Vehicle& target) override;
+
+};
+
+} // namespace Controllers
+
+#endif // SRC_INFINITE_LQR_LINEAR_TRACKING_H_
